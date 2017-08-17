960b1731c18ea48e01507ea2efa43763474a475c
Jagroop Singh <geek.jagroop@gmail.com>
2017-08-17 10:07:36 +0530

    commit by jagroop

--- application/controllers/LogOut.php
+++ application/controllers/LogOut.php
@@ -5,7 +5,11 @@ class LogOut extends App_Controller {
   public function __construct(){
       parent ::__construct(); 
    }
-   
+  
+  /**
+   * Documentation is good practice <jagroop>
+   * @return [type] [description]
+   */
   public function index(){
     $this->session->sess_destroy();
     return redirect(base_url() . 'login' );
